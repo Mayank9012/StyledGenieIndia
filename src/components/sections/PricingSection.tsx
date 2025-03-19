c0897d495ed72ec0c0330495dfd37a1048d20aa6
Mayank Tanwar <166604439+Mayank9012@users.noreply.github.com>
2025-03-19 12:05:11 +0530

    Update PricingSection.tsx

--- src/components/sections/PricingSection.tsx
+++ src/components/sections/PricingSection.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from 'next/image';
 import Container from '@/components/ui/Container';
 import { typography } from '@/styles/typography';
 import Button from '@/components/ui/Button';
